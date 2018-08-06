ee5934b8672f2ad05c04c82ff975a4d1521dda82
Amy Silva <amy_silva@me.com>
2018-08-06 12:18:28 -0700

    storing feeds into array

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -58,6 +58,7 @@ $(function() {
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
       const feed = document.querySelector('.feed');
+      const firstFeed = [];
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
